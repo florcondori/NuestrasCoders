a15827d4a0a12438debd433147c9c57aa91d24d8
flor condori <condorihuamanif@gmail.com>
2017-04-19 13:29:58 -0500

    estructura final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,31 @@
-app
+window.addEventListener("load",function(){
+	var arrayImagenes = [{name:"Aida Sulca", url:"aida.png"},
+						{name:"Angie Condor", url:"angie.png"},
+						{name:"Maia Rojas", url:"maia.png"},
+						{name:"Luz Milagros Gutierrez", url:"mimi.png"},
+						{name:"Nakarid Jave", url:"naka.png"},
+						{name:"Sandra Solorzano", url:"sandra.png"},];
+
+	document.getElementById("contenedor").appendChild(cargarImagenes(arrayImagenes));
+});
+
+function cargarImagenes(array){
+	var documentFragment = document.createDocumentFragment();
+	var ruta = "assets/img/";
+	array.forEach(function(elem){
+		var divContenedor = document.createElement("div");
+		divContenedor.classList.add("div-contenedor");
+		var img = document.createElement("img");
+		img.setAttribute("src", ruta+elem.url);
+		img.setAttribute("alt", elem.name);
+		var span = document.createElement("span");
+		span.innerText = elem.name;
+
+		divContenedor.appendChild(img);
+		divContenedor.appendChild(span);
+
+		documentFragment.appendChild(divContenedor);
+	});
+
+	return documentFragment;
+}
